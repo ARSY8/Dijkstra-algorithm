535ff4cbec8ab664e8ef98d09b078f1c8987b107
Арсений Юдинцев <arsenijudincev30350@gmail.com>
2025-05-14 20:07:45 +0700

    почти готово

--- Dijkstra algorithm/Dijkstra.cpp
+++ Dijkstra algorithm/Dijkstra.cpp
@@ -1,3 +1,40 @@
+#include <set>
+#include <limits>
+#include "Dijkstra.hpp"
 
+
+using std::set;
+
+Dijkstra::Dijkstra(unordered_map<string, vector<pair<string, unsigned int>>>& adjList_, string start_) : adjList(adjList_){
+	start = start_;
+	dijkstra();
+}
+
+unordered_map<string, unsigned int> Dijkstra::getPath() {
+	return dist;
+}
+
+void Dijkstra::dijkstra() {
+	for (const auto& v : adjList) {
+		dist[v.first] = std::numeric_limits<unsigned int>::max();
+	}
+	dist[start] = 0;
+
+	set<pair<unsigned int, string>> unvisitedVertices;
+	unvisitedVertices.insert({ dist[start], start });
+
+	while (!unvisitedVertices.empty()) {
+		string nearest = unvisitedVertices.begin()->second;
+		unvisitedVertices.erase(unvisitedVertices.begin());
+
+		for (auto& [to, weight] : adjList[nearest]) {
+			if (dist[to] > dist[nearest] + weight) {
+				unvisitedVertices.erase({ dist[to], to });
+				dist[to] = dist[nearest] + weight;
+				unvisitedVertices.insert({ dist[to], to });
+			}
+		}
+	}
+}
 
 
--- Dijkstra algorithm/Dijkstra.hpp
+++ Dijkstra algorithm/Dijkstra.hpp
@@ -1,3 +1,25 @@
-#pragma once
+#include <unordered_map>
+#include <vector>
+#include <string>
 
+
+using std::unordered_map;
+using std::vector;
+using std::string;
+using std::pair;
+
+
+class Dijkstra {
+	unordered_map<string, vector<pair<string, unsigned int>>>& adjList;
+	unordered_map<string, unsigned int> dist;
+	string start;
+
+public:
+	Dijkstra(unordered_map<string, vector<pair<string, unsigned int>>>& adjList_, string start_);
+
+	unordered_map<string, unsigned int> getPath();
+
+private:
+	void dijkstra();
+};
 
