094e3c1eb06e6e7f5c0310c3b73c4950a52a9cc6
A. Yudintsev <arsenijudincev30350@gmail.com>
2025-05-22 06:50:49 +0700

    Update FileReaderWriter.hpp

--- Dijkstra algorithm/FileReaderWriter.hpp
+++ Dijkstra algorithm/FileReaderWriter.hpp
@@ -8,7 +8,6 @@ class ReaderWriter final{
 	ReaderWriter() = delete;
 	~ReaderWriter() = delete;
 public:
-	static std::unordered_map<std::string, std::vector<std::pair<std::string, unsigned int>>> fileReader
-																			(const std::string& fileName);
+	static std::unordered_map<std::string, std::vector<std::pair<std::string, unsigned int>>> fileReader (const std::string& fileName);
 	static void fileWriter(const std::pair<std::vector<std::string>, unsigned int>& data, std::ofstream& ofs);
 };
