6d23705de17ccb3b98fcb20e639dc07a071db221
A. Yudintsev <arsenijudincev30350@gmail.com>
2025-05-22 18:07:20 +0700

    Update DijkstraTests.cpp

--- Dijkstra algorithm/DijkstraTests.cpp
+++ Dijkstra algorithm/DijkstraTests.cpp
@@ -1,37 +1,52 @@
 #include <gtest/gtest.h>
-#include "Dijkstra.hpp"
-#include <unordered_map>
-#include <vector>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <filesystem>
+#include "Dijkstra.hpp"
+#include "FileReaderWriter.hpp"
 
+namespace fs = std::filesystem;
 
-using std::unordered_map;
-using std::vector;
-using std::string;
-using std::pair;
+class DijkstraTest : public ::testing::Test {
+protected:
+    std::string inputFile = "test_graph.txt";
+    std::string outputFile = "test_output.txt";
 
-TEST(DijkstraTest, ShortestPathFromMoscow) {
-    std::unordered_map<std::string, std::vector<std::pair<std::string, unsigned int>>> graph;
+    void SetUp() override {
+        std::ofstream ofs(inputFile);
+        ofs << "Moscow Novosibirsk 7\n";
+        ofs << "Moscow Toronto 9\n";
+        ofs << "Moscow Krasnoyarsk 14\n";
+        ofs << "Novosibirsk Toronto 10\n";
+        ofs << "Novosibirsk Omsk 15\n";
+        ofs << "Omsk Toronto 11\n";
+        ofs << "Toronto Krasnoyarsk 2\n";
+        ofs << "Krasnoyarsk Kiev 9\n";
+        ofs << "Kiev Omsk 6\n";
+        ofs.close();
+    }
 
-    graph["Moscow"].push_back({"Novosibirsk", 7});
-    graph["Novosibirsk"].push_back({"Moscow", 7});
-    graph["Moscow"].push_back({"Toronto", 9});
-    graph["Toronto"].push_back({"Moscow", 9});
-    graph["Moscow"].push_back({"Krasnoyarsk", 14});
-    graph["Krasnoyarsk"].push_back({"Moscow", 14});
-    graph["Novosibirsk"].push_back({"Toronto", 10});
-    graph["Toronto"].push_back({"Novosibirsk", 10});
-    graph["Novosibirsk"].push_back({"Omsk", 15});
-    graph["Omsk"].push_back({"Novosibirsk", 15});
-    graph["Omsk"].push_back({"Toronto", 11});
-    graph["Toronto"].push_back({"Omsk", 11});
-    graph["Toronto"].push_back({"Krasnoyarsk", 2});
-    graph["Krasnoyarsk"].push_back({"Toronto", 2});
-    graph["Krasnoyarsk"].push_back({"Kiev", 9});
-    graph["Kiev"].push_back({"Krasnoyarsk", 9});
-    graph["Kiev"].push_back({"Omsk", 6});
-    graph["Omsk"].push_back({"Kiev", 6});
+    void TearDown() override {
+        fs::remove(inputFile);
+        fs::remove(outputFile);
+    }
 
+    std::vector<std::string> readFileLines(const std::string& filename) {
+        std::ifstream file(filename);
+        std::vector<std::string> lines;
+        std::string line;
+        while (std::getline(file, line)) {
+            lines.push_back(line);
+        }
+        return lines;
+    }
+};
+
+TEST_F(DijkstraTest, ShortestPathFromMoscowToKiev) {
+
+    auto graph = ReaderWriter::fileReader(inputFile);
     Dijkstra dijkstra(graph, "Moscow", "Kiev");
 
     auto pathResult = dijkstra.getPath();
@@ -39,7 +54,6 @@ TEST(DijkstraTest, ShortestPathFromMoscow) {
     auto totalDistance = pathResult.second;
 
     EXPECT_EQ(totalDistance, 20);
-
     EXPECT_EQ(road.size(), 4);
     EXPECT_EQ(road[0], "Moscow");
     EXPECT_EQ(road[1], "Toronto");
@@ -47,28 +61,8 @@ TEST(DijkstraTest, ShortestPathFromMoscow) {
     EXPECT_EQ(road[3], "Kiev");
 }
 
-TEST(DijkstraTest, ShortestPathFromToronto) {
-    std::unordered_map<std::string, std::vector<std::pair<std::string, unsigned int>>> graph;
-
-    graph["Moscow"].push_back({"Novosibirsk", 7});
-    graph["Novosibirsk"].push_back({"Moscow", 7});
-    graph["Moscow"].push_back({"Toronto", 9});
-    graph["Toronto"].push_back({"Moscow", 9});
-    graph["Moscow"].push_back({"Krasnoyarsk", 14});
-    graph["Krasnoyarsk"].push_back({"Moscow", 14});
-    graph["Novosibirsk"].push_back({"Toronto", 10});
-    graph["Toronto"].push_back({"Novosibirsk", 10});
-    graph["Novosibirsk"].push_back({"Omsk", 15});
-    graph["Omsk"].push_back({"Novosibirsk", 15});
-    graph["Omsk"].push_back({"Toronto", 11});
-    graph["Toronto"].push_back({"Omsk", 11});
-    graph["Toronto"].push_back({"Krasnoyarsk", 2});
-    graph["Krasnoyarsk"].push_back({"Toronto", 2});
-    graph["Krasnoyarsk"].push_back({"Kiev", 9});
-    graph["Kiev"].push_back({"Krasnoyarsk", 9});
-    graph["Kiev"].push_back({"Omsk", 6});
-    graph["Omsk"].push_back({"Kiev", 6});
-
+TEST_F(DijkstraTest, ShortestPathFromTorontoToKiev) {
+    auto graph = ReaderWriter::fileReader(inputFile);
     Dijkstra dijkstra(graph, "Toronto", "Kiev");
 
     auto pathResult = dijkstra.getPath();
@@ -76,9 +70,31 @@ TEST(DijkstraTest, ShortestPathFromToronto) {
     auto totalDistance = pathResult.second;
 
     EXPECT_EQ(totalDistance, 11);
-
     EXPECT_EQ(road.size(), 3);
     EXPECT_EQ(road[0], "Toronto");
     EXPECT_EQ(road[1], "Krasnoyarsk");
     EXPECT_EQ(road[2], "Kiev");
+}
+
+TEST_F(DijkstraTest, NoPathFromMoscowToLondon) {
+
+    auto graph = ReaderWriter::fileReader(inputFile);
+
+    EXPECT_ANY_THROW(Dijkstra dijkstra(graph, "Moscow", "London"));
+}
+
+TEST_F(DijkstraTest, NoPathDueToDisconnectedGraph) {
+
+    std::ofstream ofs(inputFile);
+    ofs << "Moscow Novosibirsk 7\n";
+    ofs << "London Minsk 9\n";
+    ofs << "Moscow Toronto 9\n";
+    ofs << "Toronto Krasnoyarsk 2\n";
+    ofs << "Krasnoyarsk Omsk 15\n";
+    ofs.close();
+
+    auto graph = ReaderWriter::fileReader(inputFile);
+    Dijkstra dijkstra(graph, "London", "Novosibirsk");
+
+    EXPECT_ANY_THROW(dijkstra.getPath());
 }
